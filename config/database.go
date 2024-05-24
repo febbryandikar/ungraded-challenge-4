8e195440e4d7830be2f8fffea1ecf19bde3a76ee
febbryandikar <faramadhan@students.hacktiv8.ac.id>
2024-05-24 17:30:22 +0700

    fix: fix code

--- config/database.go
+++ config/database.go
@@ -16,10 +16,11 @@ func GetDatabase() *sql.DB {
 		log.Fatal("Error loading .env file")
 	}
 
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s",
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
 		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME")))
 	if err != nil {
 		fmt.Println(err.Error())
--- config/server.go
+++ config/server.go
@@ -2,14 +2,16 @@ package config
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func SetupServer() (*httprouter.Router, *http.Server) {
 	router := httprouter.New()
+	port := os.Getenv("PORT")
 	server := http.Server{
-		Addr:    "localhost:8080",
+		Addr:    ":" + port,
 		Handler: router,
 	}
 	return router, &server
